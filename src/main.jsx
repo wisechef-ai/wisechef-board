92a30f9d01753a8b9d4d346c76c91656d6f33884
wisechef-hq <backup@wisechef.ai>
2026-02-26 20:41:02 +0000

    fix: reference __BUILD_TS__ to force unique hash per build

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+/* eslint-disable no-unused-vars */
+const _buildTs = typeof __BUILD_TS__ !== 'undefined' ? __BUILD_TS__ : 0
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
